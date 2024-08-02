31dc26872cd131aea2e1232ded514b412582ad0e
quadrovicente28 <118233540+quadrovicente28@users.noreply.github.com>
2024-08-03 07:17:32 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -40,7 +40,7 @@ const Footer = () => {
               <a href={profile.link}>
                 <img
                   src={profile.img}
-                  alt={profile.id}
+                  alt={profile.img}
                   width={20}
                   height={20}
                 />
