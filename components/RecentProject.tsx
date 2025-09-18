3db43675f698e2a20cb57401d515708337446b16
quadrovicente28 <118233540+quadrovicente28@users.noreply.github.com>
2025-09-18 19:54:04 +0800

    update

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -8,7 +8,7 @@ const RecentProject = () => {
     <section className="py-20" id="projects">
       <h1 className="heading">
         A small selections of{" "}
-        <span className="text-purple">recent projects</span>
+        <span className="text-purple">recent CMS projects</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
